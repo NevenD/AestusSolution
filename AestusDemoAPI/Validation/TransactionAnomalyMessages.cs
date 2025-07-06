f29f67e303509ffba682bff464ac4a071e409cd8
Neven Dimač <neven.dimac@gmail.com>
2025-07-07 00:27:01 +0200

    Fix anomaly messages

--- AestusDemoAPI/Validation/TransactionAnomalyMessages.cs
+++ AestusDemoAPI/Validation/TransactionAnomalyMessages.cs
@@ -2,7 +2,7 @@
 {
     public static class TransactionAnomalyMessages
     {
-        public const string ExpectedAmount = "Normalna potrošnja";
+        public const string ExpectedAmount = "Normalna potrošnja.";
         public const string FrequencySpike = "Otkriven nagli porast frekvencije transakcija.";
         public const string IQRAnomaly = "Otkrivena anomalija unutar interkvartilnog raspona.";
         public const string UnexpectedAmount = "Neočekivan iznos transakcije.";
