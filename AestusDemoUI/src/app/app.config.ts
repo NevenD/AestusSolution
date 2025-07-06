d3fe8c8ed5c27b17cb1e54f2e6fd827311f2c966
Neven Dimač <neven.dimac@gmail.com>
2025-07-06 23:46:53 +0200

    Installed charts

--- AestusDemoUI/src/app/app.config.ts
+++ AestusDemoUI/src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
-    provideToastr(),
+    provideToastr(), provideCharts(withDefaultRegisterables()),
   ],
 };
